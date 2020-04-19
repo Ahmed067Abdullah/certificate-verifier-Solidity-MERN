623de1a92fd45bc16cd572f114d1b7573c8e46a7
Ahmed Abdullah <ahmed@sudofy.com>
2020-04-19 17:54:25 +0500

    checks for existing organization and certificate IDs added

--- CertificateVerifier.sol
+++ CertificateVerifier.sol
@@ -5,6 +5,7 @@ contract CertificateVerifier {
         string name;
         string logoURL;
         string website;
+        bool isSet;
     }
     
     struct Certificate {
@@ -13,13 +14,15 @@ contract CertificateVerifier {
         string startDate;
         string endDate;
         address awarder;
+        bool isSet;
     }
     
     mapping (address => Company) Companies;
     mapping (string => Certificate) Certificates;
     
     function registerCompany(string memory name, string memory  logoURL, string memory  website) public returns (string memory) {
-        Companies[tx.origin] = Company(name, logoURL, website);
+        require(!Companies[tx.origin].isSet, "Address already in use");
+        Companies[tx.origin] = Company(name, logoURL, website, true);
     }
     
     function getCompany(address companyAddress) public view returns (string memory) {
@@ -27,7 +30,9 @@ contract CertificateVerifier {
     }
     
     function awardCertificate(string memory uuid, string memory name, string memory  position, string memory startDate, string memory  endDate) public {
-        Certificates[uuid] = Certificate(name, position, startDate, endDate, tx.origin);
+        require(Companies[tx.origin].isSet, "Organization isn't registered");
+        require(!Certificates[uuid].isSet, 'Certificate ID already used');
+        Certificates[uuid] = Certificate(name, position, startDate, endDate, tx.origin, true);
     }
     
     function getCertificate(string memory uuid) public view returns (string memory) {
