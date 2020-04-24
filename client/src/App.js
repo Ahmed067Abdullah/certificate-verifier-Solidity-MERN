bfb266404d22bd32cc5b25979e96c4f61a1bf51d
Ahmed Abdullah <ahmed@sudofy.com>
2020-04-24 12:31:06 +0500

    basic routing added

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import ReactDOMServer from 'react-dom/server';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import ViewCertificate from './containers/view-certificate/ViewCertificate';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <ViewCertificate />
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/about" component={ViewCertificate} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
