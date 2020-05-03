f15a84e5f4d919f75c1340ad65507ad330e544db
Ahmed Abdullah <ahmed@sudofy.com>
2020-05-03 15:21:10 +0500

    read basic company data from contract

--- client/src/containers/register-company/RegisterCompany.js
+++ client/src/containers/register-company/RegisterCompany.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Form, Input, Button, Card } from 'antd';
 import Navbar from '../../components/nav-bar/NavBar';
+import web3 from '../../shared/web3';
+import contract from '../../shared/contract';
 import { registerCompanyFormFields as formFields } from '../../shared/formFields';
 import { layout, tailLayout } from '../../shared/formLayout';
 import stylesheet from './RegisterCompany.styles';
-import { SketchPicker } from 'react-color';
+// import { SketchPicker } from 'react-color';
 
 const RegisterCompany = () => {
+  useEffect(() => {
+    console.log('here', web3.eth.accounts.wallet[0])
+    contract.methods.getCompany('0x6b19bD093F7D98505d4b31aF5F8DBF19d7f1c542')
+      .call({ from: '0x6b19bD093F7D98505d4b31aF5F8DBF19d7f1c542' })
+      .then(res => {
+        console.log('here', res)
+      })
+      .catch(err => {
+        console.log(err)
+      });
+  }, []);
+  
   const onFinish = values => {
     console.log('Success:', values);
   };
@@ -15,9 +29,9 @@ const RegisterCompany = () => {
     console.log('Failed:', errorInfo);
   };
 
-  const handleChangeComplete = (color) => {
-    console.log(color);
-  };
+  // const handleChangeComplete = (color) => {
+  //   console.log(color);
+  // };
 
   const classes = stylesheet();
 
