d6993ecaba1ac295b48002d6a1d28fe5e29ab03e
Ahmed Abdullah <ahmed@sudofy.com>
2020-05-05 18:05:40 +0500

    use address of metamask

--- client/src/containers/register-company/RegisterCompany.js
+++ client/src/containers/register-company/RegisterCompany.js
@@ -1,7 +1,7 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Form, Input, Button, Card } from 'antd';
 import Navbar from '../../components/nav-bar/NavBar';
-import web3 from '../../shared/web3';
+// import web3 from '../../shared/web3';
 import contract from '../../shared/contract';
 import { registerCompanyFormFields as formFields } from '../../shared/formFields';
 import { layout, tailLayout } from '../../shared/formLayout';
@@ -9,24 +9,45 @@ import stylesheet from './RegisterCompany.styles';
 // import { SketchPicker } from 'react-color';
 
 const RegisterCompany = () => {
+  const [isMetaMaskEnabled, setIsMetaMaskEnabled] = useState(false);
   useEffect(() => {
-    console.log('here', web3.eth.accounts.wallet[0])
-    contract.methods.getCompany('0x6b19bD093F7D98505d4b31aF5F8DBF19d7f1c542')
-      .call({ from: '0x6b19bD093F7D98505d4b31aF5F8DBF19d7f1c542' })
+    const { ethereum } = window;
+    if (!ethereum) {
+      alert('Please install metamask');
+    }
+    ethereum.enable()
+      .then(() => {
+        setIsMetaMaskEnabled(true);
+      });
+
+    contract.methods.getCompany('0x4Af3462EdE5F27469cF2Ef9F590947f0648dDecf')
+      .call({ from: ethereum.selectedAddress })
       .then(res => {
-        console.log('here', res)
+        console.log('Comapny:', res)
       })
       .catch(err => {
         console.log(err)
       });
   }, []);
-  
-  const onFinish = values => {
-    console.log('Success:', values);
-  };
 
-  const onFinishFailed = errorInfo => {
-    console.log('Failed:', errorInfo);
+  const onFinish = values => {
+    const { company, website, logo } = values;
+    const { ethereum } = window;
+    console.log(ethereum.selectedAddress)
+    contract.methods.registerCompany(company, logo, website)
+      .call({
+        from: ethereum.selectedAddress,
+        gas: 1000000,
+        gasPrice: '30000000000000',
+      }, (a, b, c) => {
+        console.log(a, b, c);
+      })
+      .then(res => {
+        console.log('reg cmp: ', res)
+      })
+      .catch(err => {
+        console.log(err)
+      });
   };
 
   // const handleChangeComplete = (color) => {
@@ -45,7 +66,6 @@ const RegisterCompany = () => {
           {...layout}
           name="register-comapny"
           onFinish={onFinish}
-          onFinishFailed={onFinishFailed}
         >
           {formFields.map(field => <Form.Item
             key={field.name}
@@ -60,7 +80,7 @@ const RegisterCompany = () => {
           onChangeComplete={handleChangeComplete}
         /> */}
           <Form.Item {...tailLayout}>
-            <Button type="primary" htmlType="submit">
+            <Button type="primary" htmlType="submit" disabled={!isMetaMaskEnabled}>
               Submit
         </Button>
           </Form.Item>
--- client/src/shared/contract.js
+++ client/src/shared/contract.js
@@ -3,106 +3,106 @@ import web3 from './web3';
 const address = '0x97882b6527ffb26cbf7ddc67a906d13390b7b66e';
 
 const abi = [
-	{
-		"inputs": [
-			{
-				"internalType": "string",
-				"name": "uuid",
-				"type": "string"
-			},
-			{
-				"internalType": "string",
-				"name": "name",
-				"type": "string"
-			},
-			{
-				"internalType": "string",
-				"name": "position",
-				"type": "string"
-			},
-			{
-				"internalType": "string",
-				"name": "startDate",
-				"type": "string"
-			},
-			{
-				"internalType": "string",
-				"name": "endDate",
-				"type": "string"
-			}
-		],
-		"name": "awardCertificate",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "string",
-				"name": "name",
-				"type": "string"
-			},
-			{
-				"internalType": "string",
-				"name": "logoURL",
-				"type": "string"
-			},
-			{
-				"internalType": "string",
-				"name": "website",
-				"type": "string"
-			}
-		],
-		"name": "registerCompany",
-		"outputs": [
-			{
-				"internalType": "string",
-				"name": "",
-				"type": "string"
-			}
-		],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "string",
-				"name": "uuid",
-				"type": "string"
-			}
-		],
-		"name": "getCertificate",
-		"outputs": [
-			{
-				"internalType": "string",
-				"name": "",
-				"type": "string"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "address",
-				"name": "companyAddress",
-				"type": "address"
-			}
-		],
-		"name": "getCompany",
-		"outputs": [
-			{
-				"internalType": "string",
-				"name": "",
-				"type": "string"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	}
+  {
+    "inputs": [
+      {
+        "internalType": "string",
+        "name": "uuid",
+        "type": "string"
+      },
+      {
+        "internalType": "string",
+        "name": "name",
+        "type": "string"
+      },
+      {
+        "internalType": "string",
+        "name": "position",
+        "type": "string"
+      },
+      {
+        "internalType": "string",
+        "name": "startDate",
+        "type": "string"
+      },
+      {
+        "internalType": "string",
+        "name": "endDate",
+        "type": "string"
+      }
+    ],
+    "name": "awardCertificate",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "string",
+        "name": "name",
+        "type": "string"
+      },
+      {
+        "internalType": "string",
+        "name": "logoURL",
+        "type": "string"
+      },
+      {
+        "internalType": "string",
+        "name": "website",
+        "type": "string"
+      }
+    ],
+    "name": "registerCompany",
+    "outputs": [
+      {
+        "internalType": "string",
+        "name": "",
+        "type": "string"
+      }
+    ],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "string",
+        "name": "uuid",
+        "type": "string"
+      }
+    ],
+    "name": "getCertificate",
+    "outputs": [
+      {
+        "internalType": "string",
+        "name": "",
+        "type": "string"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "companyAddress",
+        "type": "address"
+      }
+    ],
+    "name": "getCompany",
+    "outputs": [
+      {
+        "internalType": "string",
+        "name": "",
+        "type": "string"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  }
 ];
 
 export default new web3.eth.Contract(abi, address);
