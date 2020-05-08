5e7f5d96954c10367a4508aa69a6dc3379048961
Ahmed Abdullah <ahmed@sudofy.com>
2020-05-08 17:39:30 +0500

    address validation for company regestration done

--- client/src/containers/register-company/RegisterCompany.js
+++ client/src/containers/register-company/RegisterCompany.js
@@ -5,10 +5,17 @@ import { registerCompanyFormFields as formFields } from '../../shared/formFields
 import { layout, tailLayout } from '../../shared/formLayout';
 import stylesheet from './RegisterCompany.styles';
 import { registerCompany } from './RegisterCompany.service';
+import { checkCompany } from '../award-certificate/AwardCertificate.service';
 // import { SketchPicker } from 'react-color';
 
 const RegisterCompany = () => {
   const [isSubmitting, setIsSubmitting] = useState(false);
+  const [comapnyLoading, setCompanyLoading] = useState(true);
+  const [comapnyNotRegistered, setComapnyNotRegistered] = useState(true);
+
+  useEffect(() => {
+    checkCompany(setCompanyLoading, setComapnyNotRegistered);
+  }, []);
 
   const onFinish = values => {
     setIsSubmitting(true);
@@ -30,12 +37,20 @@ const RegisterCompany = () => {
 
   const classes = stylesheet();
 
+  const companyAlreadyRegistered = !comapnyNotRegistered;
+
   return (
     <div style={{ 'pointerEvents': isSubmitting ? 'none' : 'all' }}>
       <Navbar />
       <Card
         className={classes['register-company-card']}
-        title="Register Company">
+        title="Register Company"
+        loading={comapnyLoading}>
+        {companyAlreadyRegistered
+          ? <p className={classes['cmp-unregistered']}>
+            Sorry, the selected Ethereum address is already associated with a company.
+          </p>
+          : null}
         <Form
           {...layout}
           name="register-comapny"
@@ -47,14 +62,14 @@ const RegisterCompany = () => {
             name={field.name}
             rules={field.rules}
           >
-            <Input disabled={isSubmitting} />
+            <Input disabled={isSubmitting || companyAlreadyRegistered} />
           </Form.Item>)}
           {/* <SketchPicker
           onChange={e => console.log(e)}
           onChangeComplete={handleChangeComplete}
         /> */}
           <Form.Item {...tailLayout}>
-            <Button type="primary" htmlType="submit" loading={isSubmitting}>
+            <Button type="primary" htmlType="submit" loading={isSubmitting} disabled={companyAlreadyRegistered}>
               Submit
         </Button>
           </Form.Item>
--- client/src/containers/register-company/RegisterCompany.styles.js
+++ client/src/containers/register-company/RegisterCompany.styles.js
@@ -1,6 +1,12 @@
 import { createUseStyles } from 'react-jss';
 
 const styles = {
+  'cmp-unregistered': {
+    'fontSize': '16px',
+    'color': '#ff4d4f',
+    'fontWeight': '500',
+    'textAlign': 'center'
+  },
   'register-company-card': {
     width: 700, margin: '100px auto 0'
   }
