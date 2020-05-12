c149f5f72740874f94e87b37dd406cec3040b38b
Ahmed Abdullah <ahmed@sudofy.com>
2020-05-12 17:21:17 +0500

    API integration for award certificate

--- client/src/containers/award-certificate/AwardCertificate.service.js
+++ client/src/containers/award-certificate/AwardCertificate.service.js
@@ -1,6 +1,7 @@
 import contract from '../../shared/contract';
 import { v4 as uuidv4 } from 'uuid';
 import moment from 'moment';
+import axios from 'axios';
 
 export const checkCompany = (setCompanyLoading, setComapnyNotRegistered, setCompany = () => { }) => {
   const { ethereum } = window;
@@ -21,27 +22,56 @@ export const checkCompany = (setCompanyLoading, setComapnyNotRegistered, setComp
   }
 }
 
-export const awardCertificate = (values) => {
-  const { candidateName, duration, position, presenter, presenterDesignation } = values;
-  const { ethereum } = window;
-  const uuid = uuidv4();
-  const startDate = moment(duration[0]).unix().toString();
-  const endDate = moment(duration[1]).unix().toString();
+export const saveCertificate = async payload => {
+  await axios.post('http://localhost:5000/api/certificates', payload);
+};
 
-  return contract.methods.awardCertificate(
-    uuid,
-    candidateName,
-    position,
-    startDate,
-    endDate,
-    presenter,
-    presenterDesignation
-  )
-    .send({ from: ethereum.selectedAddress }, (err, address) => {
-      if (err) {
-        console.log(err);
-        return;
+export const updatedCertificateStatus = async (_id, status) => {
+  await axios.put(`http://localhost:5000/api/certificates${_id}`, { status });
+};
+
+
+export const awardCertificate = values => {
+  return new Promise(async (resolve, reject) => {
+    const { candidateName, duration, position, presenter, presenterDesignation } = values;
+    const { ethereum } = window;
+    const uuid = uuidv4();
+    const startDate = moment(duration[0]).unix().toString();
+    const endDate = moment(duration[1]).unix().toString();
+
+    let certificate = null;
+    try {
+      certificate = await saveCertificate({
+        uuid,
+        companyAddress: ethereum.selectedAddress,
+        candidateName
+      });
+
+      await contract.methods.awardCertificate(
+        uuid,
+        candidateName,
+        position,
+        startDate,
+        endDate,
+        presenter,
+        presenterDesignation
+      )
+        .send({ from: ethereum.selectedAddress }, (err, address) => {
+          if (err) {
+            console.log(err);
+            return;
+          }
+          console.log('Tx Address:', address);
+        });
+
+      await updatedCertificateStatus(certificate._id, 1);
+      resolve();
+    } catch (e) {
+      if (certificate) {
+        await updatedCertificateStatus(certificate._id, 2)
       }
-      console.log('Tx Address:', address);
-    });
+      console.log(e);
+      reject(e);
+    }
+  })
 }
