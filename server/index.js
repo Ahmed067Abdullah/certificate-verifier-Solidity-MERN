4e10bf78fca97dca8e01c728745a7714b2b15c86
Ahmed Abdullah <ahmed@sudofy.com>
2020-05-16 16:16:31 +0500

    prepare to deploy

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const path = require("path");
 const cors = require("cors");
 
 const certificate = require("./entities/certificate/Certificate.route");
@@ -18,6 +19,14 @@ mongoose
   .catch(err => console.log("Error Occured while connecting to DB", err));
 
 app.use("/api/certificates", certificate);
+
+// for production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("../client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "..", "client", "build", "index.html"));
+  });
+}
 
 app.listen(port, () => {
   console.log(`server started on port ${port}`);
