6c450748c4c0ba94487d9811ced7cb2135b99546
Ahmed067Abdullah <ahmed@sudofy.com>
2020-05-30 11:52:56 +0500

    register company loading forever fix

--- client/src/containers/register-company/RegisterCompany.service.js
+++ client/src/containers/register-company/RegisterCompany.service.js
@@ -3,18 +3,25 @@ import contract from '../../shared/contract';
 export const registerCompany = (values) => {
   const { company, website, logo, primaryColor, secondaryColor } = values;
   const { ethereum } = window;
-  return contract.methods.registerCompany(
-    company,
-    logo,
-    website,
-    primaryColor ? primaryColor : '#1890ff',
-    secondaryColor ? secondaryColor : '#000000a6'
-  )
-    .send({ from: ethereum.selectedAddress }, (err, address) => {
-      if (err) {
-        console.log(err);
-        return;
-      }
-      console.log('Tx Address:', address);
-    })
+  return new Promise(async (resolve, reject) => {
+    try {
+      await contract.methods.registerCompany(
+        company,
+        logo,
+        website,
+        primaryColor ? primaryColor : '#1890ff',
+        secondaryColor ? secondaryColor : '#000000a6'
+      )
+        .send({ from: ethereum.selectedAddress }, (err, address) => {
+          if (err) {
+            console.log(err);
+            return;
+          }
+          console.log('Tx Address:', address);
+        })
+      resolve();
+    } catch (e) {
+      reject(e);
+    }
+  })
 };
